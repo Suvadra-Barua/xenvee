02dce2fa4068d4cc416a94f6d4c62a9f629972d7
Suvadra-Barua <suvadra@universalmachine.io>
2024-09-02 11:03:26 +0600

    Update volunteer experience and MyLinks

--- src/app/data/ExperienceTab.ts
+++ src/app/data/ExperienceTab.ts
@@ -1,14 +1,14 @@
 const professional_work = [
-    {
-      company: "Universal Machine Inc.",
-      href: "https://atomic.finance",
-      badges: [],
-      location: "Silicon Valley, California",
-      title: "Blockchain Engineer",
-      logoUrl: "/atomic.png",
-      start: "November 2021",
-      end: "Running",
-      description: `<li>Designed, developed & shipped several projects related to NFT, Blockchain & Web3</li>
+  {
+    company: "Universal Machine Inc.",
+    href: "https://atomic.finance",
+    badges: [],
+    location: "Silicon Valley, California",
+    title: "Blockchain Engineer",
+    logoUrl: "/atomic.png",
+    start: "November 2021",
+    end: "Running",
+    description: `<li>Designed, developed & shipped several projects related to NFT, Blockchain & Web3</li>
 <li>Implemented smart contracts with a focus on maintainability, usability, scalability, and performance</li>
 <li>Facilitated Front End integration with Smart Contracts</li>
 <li>Developed Smart Contracts with Solidity (ERC20, ERC721, ERC777, ERC1155)</li>
@@ -18,116 +18,125 @@ const professional_work = [
 <li>Audited & assessed smart contracts for adherence to security standards (SWC Registry)</li>
 <li>Designed & developed crypto accounting application for Celo with multiple wallet integrations</li>
 <li>Engaged in full-stack development (frontend, backend, and Web3), demonstrating versatility and a commitment to mastering new technologies in the blockchain space.</li>`,
-    },
-    {
-      company: "Creative Crowdfunding Protocol",
-      href: "https://splunk.com",
-      badges: [],
-      location: "USA",
-      title: "Software Engineer",
-      logoUrl: "/splunk.svg",
-      start: "Nov 2023 ",
-      end: "Running",
-      description: `<li>Conducted research on various on-ramp and off-ramp platforms to select the most suitable one for our project</li>
+  },
+  {
+    company: "Creative Crowdfunding Protocol",
+    href: "https://splunk.com",
+    badges: [],
+    location: "USA",
+    title: "Software Engineer",
+    logoUrl: "/splunk.svg",
+    start: "Nov 2023 ",
+    end: "Running",
+    description: `<li>Conducted research on various on-ramp and off-ramp platforms to select the most suitable one for our project</li>
 <li>Tested smart contracts and implemented a frontend for demonstration purposes</li>
 <li>Developed the backend for the wallet service, providing APIs for operations related to custodial wallets</li>`,
-    },
-    {
-      company: "Silicon Orchard Ltd.",
-      href: "https://splunk.com",
-      badges: [],
-      location: "Dhaka, Bangladesh",
-      title: "Blockchain Intern",
-      logoUrl: "/splunk.svg",
-      start: "Aug 2021 ",
-      end: "Oct 2021",
-      description: `<li>Implemented multiple NFT projects & marketplaces</li>
+  },
+  {
+    company: "Silicon Orchard Ltd.",
+    href: "https://splunk.com",
+    badges: [],
+    location: "Dhaka, Bangladesh",
+    title: "Blockchain Intern",
+    logoUrl: "/splunk.svg",
+    start: "Aug 2021 ",
+    end: "Oct 2021",
+    description: `<li>Implemented multiple NFT projects & marketplaces</li>
 <li>Developed mainly on Ethereum blockchain platform tooling</li>`,
-    },
-    {
-      company: "BJIT Ltd",
-      href: "https://li.me/",
-      badges: [],
-      location: "Dhaka, Bangladesh",
-      title: "Trainee Engineer",
-      logoUrl: "/lime.svg",
-      start: "Aug 2019",
-      end: "Oct 2019",
-      description: "",
-    },
-  ];
-  const teaching_Experince = [
-    {
-      company: "Ostad ",
-      badges: [],
-      href: "https://ostad.app/en/course/blockchain-development",
-      location: "Dhaka, Bangladesh",
-      title: "Lead Instructor",
-      logoUrl: "/shopify.svg",
-      start: "Aug 2023",
-      end: " Dec 2023",
-      description: `<li>Designed 90% of the comprehensive blockchain course module, covering fundamental to advanced concepts</li>
+  },
+  {
+    company: "BJIT Ltd",
+    href: "https://li.me/",
+    badges: [],
+    location: "Dhaka, Bangladesh",
+    title: "Trainee Engineer",
+    logoUrl: "/lime.svg",
+    start: "Aug 2019",
+    end: "Oct 2019",
+    description: "",
+  },
+];
+const teaching_Experince = [
+  {
+    company: "Ostad ",
+    badges: [],
+    href: "https://ostad.app/en/course/blockchain-development",
+    location: "Dhaka, Bangladesh",
+    title: "Lead Instructor",
+    logoUrl: "/shopify.svg",
+    start: "Aug 2023",
+    end: " Dec 2023",
+    description: `<li>Designed 90% of the comprehensive blockchain course module, covering fundamental to advanced concepts</li>
 <li>Developed engaging and interactive course materials, including lectures, assignments, and hands-on projects</li>
 <li>Led 70% of the classes, delivering high-quality instruction to a diverse group of students</li>
 <li>Collaborated with other instructors and industry professionals to ensure course content was current and relevant</li>`,
-    },
-    {
-      company: "University of Creative Technology, Chittagong",
-      href: "https://www.uctc.edu.bd/cse",
-      badges: [],
-      location: "Chattogram, Bangladesh",
-      title: "Lecturer",
-      logoUrl: "/nvidia.png",
-      start: "Jul 2022",
-      end: "Dec 2022",
-      description: `Completed coursework in:
+  },
+  {
+    company: "University of Creative Technology, Chittagong",
+    href: "https://www.uctc.edu.bd/cse",
+    badges: [],
+    location: "Chattogram, Bangladesh",
+    title: "Lecturer",
+    logoUrl: "/nvidia.png",
+    start: "Jul 2022",
+    end: "Dec 2022",
+    description: `Completed coursework in:
 <li>Social and Professional Ethics in Computing</li>
 <li>Introduction to Computing & Programming Laboratory</li>
 <li>Web Programming Laboratory</li>`,
-    },
-    {
-      company: "CUET Computer Club",
-      href: "https://nvidia.com/",
-      badges: [],
-      location: " Chattogram, Bangladesh",
-      title: "Workshop Instructor",
-      logoUrl: "/nvidia.png",
-      start: "Jul 2022",
-      end: "Dec 2022",
-      description: `
-<li>Instructed over 40 students in a three-day session titled ‘All About Blockchain & Smart Contract Development with Solidity’</li>`,
-    },
-    {
-      company: "KolpoKoushol",
-      href: "https://kolpokoushol.org/",
-      badges: [],
-      location: "Dhaka, Bangladesh",
-      title: "Mentor",
-      logoUrl: "/nvidia.png",
-      start: "Oct 2023 ",
-      end: "",
-      description: `
+  },
+  {
+    company: "CUET Computer Club",
+    href: "https://nvidia.com/",
+    badges: [],
+    location: " Chattogram, Bangladesh",
+    title: "Workshop Instructor",
+    logoUrl: "/nvidia.png",
+    start: "Jul 2022",
+    end: "Dec 2022",
+    description: `
+          Instructed a group of approximately 40+ students in a three-day session titled "All about Blockchain & Smart Contract Development on Solidity". The syllabus covered the following topics:
+
+          <li>Day 1<br>
+          Understanding Blockchain, Cryptocurrency</li>
+
+          <li>Day 2<br>
+          Blockchain Programming Fundamentals: Introduction to Solidity</li>
+
+          <li>Day 3<br>
+          Building 5 Hands-On Smart Contracts</li>`,
+  },
+  {
+    company: "KolpoKoushol",
+    href: "https://kolpokoushol.org/",
+    badges: [],
+    location: "Dhaka, Bangladesh",
+    title: "Mentor",
+    logoUrl: "/nvidia.png",
+    start: "Oct 2023 ",
+    end: "",
+    description: `
 <li>Guided mentees in web development, smart contract implementation, and transforming ideas into projects</li>`,
-    },
-  ];
-  const volunteer_experience = [
-    {
-      company: "World Blockchain Organization",
-      badges: [],
-      href: "",
-      location: "",
-      title: "Editor",
-      logoUrl: "/shopify.svg",
-      start: "Sep 2020",
-      end: "Dec 2020",
-      description: ``,
-    },
-  ];
+  },
+];
+const volunteer_experience = [
+  {
+    company: "World Blockchain Organization",
+    badges: [],
+    href: "",
+    location: "",
+    title: "Editor",
+    logoUrl: "/shopify.svg",
+    start: "Sep 2020",
+    end: "Dec 2020",
+    description: ``,
+  },
+];
 
-  const ExpericenTabPageData = {
-    professional_work,
-    teaching_Experince,
-    volunteer_experience
-  }
+const ExpericenTabPageData = {
+  professional_work,
+  teaching_Experince,
+  volunteer_experience,
+};
 
-  export default ExpericenTabPageData;
+export default ExpericenTabPageData;
--- src/components/custom/MyLinks.tsx
+++ src/components/custom/MyLinks.tsx
@@ -4,54 +4,54 @@ import {
   AiFillTwitterCircle,
   AiFillCodepenCircle,
 } from "react-icons/ai";
-import { SiGithub, SiGooglescholar,SiLeetcode, SiLinkedin  } from "react-icons/si";
+import {
+  SiGithub,
+  SiGooglescholar,
+  SiLeetcode,
+  SiLinkedin,
+} from "react-icons/si";
 
 import { motion } from "framer-motion";
 
 const links = [
   {
-    link:"https://www.linkedin.com",
-    icon: <SiLinkedin  className="size-4  md:size-6  lg:size-8 "/>
+    link: "https://www.linkedin.com/in/Suvadra-Barua/",
+    icon: <SiLinkedin className="size-4  md:size-6  lg:size-8 " />,
   },
   {
-    link:"https://www.github.com",
-    icon: <SiLeetcode className="size-4  md:size-6  lg:size-8 "/>
+    link: "https://leetcode.com/u/user4818qC/",
+    icon: <SiLeetcode className="size-4  md:size-6  lg:size-8 " />,
   },
   {
-    link:"https://www.linkedin.com",
-    icon: <SiGithub className="size-4  md:size-6  lg:size-8 "/>
+    link: "https://github.com/Suvadra-Barua",
+    icon: <SiGithub className="size-4  md:size-6  lg:size-8 " />,
   },
   {
-    link:"https://www.linkedin.com",
-    icon: <SiGooglescholar className="size-4  md:size-6  lg:size-8 "/>
+    link: "https://scholar.google.com/citations?user=haA8Z1UAAAAJ&hl=en",
+    icon: <SiGooglescholar className="size-4  md:size-6  lg:size-8 " />,
   },
-
 ];
 
 export const MyLinks = () => {
-
   return (
     <div className="flex gap-3 pt-2.5">
-      {
-        links.map((link,id)=>(
-          <motion.span
+      {links.map((link, id) => (
+        <motion.span
           key={id}
-        initial={{ y: -30, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: id * 0.2 }}      >
-        <a
-          href={link.link}
-          target="_blank"
-          rel="nofollow"
-          className="opacity-50 relative z-20 hover:text-earth-500 hover:opacity-100"
+          initial={{ y: -30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: id * 0.2 }}
         >
-          {link.icon}
-        </a>
-      </motion.span>
-        )
-        )
-      }
-
+          <a
+            href={link.link}
+            target="_blank"
+            rel="nofollow"
+            className="opacity-50 relative z-20 hover:text-earth-500 hover:opacity-100"
+          >
+            {link.icon}
+          </a>
+        </motion.span>
+      ))}
     </div>
   );
 };
