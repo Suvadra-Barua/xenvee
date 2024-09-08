f647615ed4457d1d93f6a35d4d2d35dfb11fa978
Suvadra-Barua <suvadra@universalmachine.io>
2024-09-08 20:02:54 +0600

    Add url to prestige

--- src/app/data/prestige.ts
+++ src/app/data/prestige.ts
@@ -1,40 +1,41 @@
 const publications = [
-    {
-      title:
-        "A Blockchain based Decentralized Video Streaming Platform with Content Protection System",
-      authors: "Suvadra Barua, Dipon Talukder",
-      journal:
-        "23rd International Conference on Computer and Information Technology (ICCIT)",
-      year: 2020,
-      doi: "10.1109/ICCIT51783.2020.9392746",
-    },
-    {
-      title:
-        "OkkhorNama: BdSL Image Dataset For Real Time Object Detection Algorithms",
-      authors: "Dipon Talukder, Fatima Jahara, Suvadra Barua, Md. Mokammel Haque",
-      journal: "IEEE Region 10 Symposium (TENSYMP)",
-      year: 2021,
-      doi: "10.1109/TENSYMP52854.2021.9550907",
-    },
-    // Add more publications as needed
-  ];
-  const awards = [
-    `<div><b>Silver Award | Professional Category | Blockchain Olympiad Bangladesh, 2022</b>
+  {
+    title:
+      "A Blockchain based Decentralized Video Streaming Platform with Content Protection System ",
+    authors: "Suvadra Barua, Dipon Talukder",
+    journal:
+      "23rd International Conference on Computer and Information Technology (ICCIT)",
+    year: 2020,
+    doi: "10.1109/ICCIT51783.2020.9392746",
+    url: "https://ieeexplore.ieee.org/document/9392746",
+  },
+  {
+    title:
+      "OkkhorNama: BdSL Image Dataset For Real Time Object Detection Algorithms",
+    authors: "Dipon Talukder, Fatima Jahara, Suvadra Barua, Md. Mokammel Haque",
+    journal: "IEEE Region 10 Symposium (TENSYMP)",
+    year: 2021,
+    doi: "10.1109/TENSYMP52854.2021.9550907",
+    url: "https://ieeexplore.ieee.org/document/9550907",
+  },
+  // Add more publications as needed
+];
+const awards = [
+  `<div><b>Silver Award | Professional Category | Blockchain Olympiad Bangladesh, 2022 </b><a href="https://bcolbd.org/2022/winner"><u>[link]</u></a>
   <div>Project : AttoEarth - Solving Land Disputes at a High Precision</div>
   Role: Blockchain Engineer</div>`,
-  `<div><b>Bronze Award | Student Category | Blockchain Olympiad Bangladesh, 2021</b>
+  `<div><b>Bronze Award | Student Category | Blockchain Olympiad Bangladesh, 2021</b> <a href="https://bcolbd.org/2021/winner"><u>[link]</u></a>
   <div>Project : TrioChain -  A Blockchain Based Passport Management System for Bangladesh</div>
   Role: Team Leader</div>`,
-    `Merit Award | Student Category | International  Blockchain Olympiad Bangladesh, 2021`,
-    `Technical Scholarship | Chittagong University of Engineering and Technology (CUET) ,2014-2018`,
-    `Secondary School Certificate (SSC) Scholarship Award | Bangladesh Education Board, 2013-2015`,
-    `Winner | Kaptai Mathematical Olympiad, 2012`,
-  ];
+  `Merit Award | Student Category | International  Blockchain Olympiad Bangladesh, 2021`,
+  `Technical Scholarship | Chittagong University of Engineering and Technology (CUET) ,2014-2018`,
+  `Secondary School Certificate (SSC) Scholarship Award | Bangladesh Education Board, 2013-2015 <a href="https://bangladeshresults.files.wordpress.com/2014/03/ssc-scholarship-2013-chittagong-board.pdf"><u>[link]</u></a>`,
+  `Winner | Kaptai Mathematical Olympiad, 2012`,
+];
 
+const prestigePageData = {
+  publications,
+  awards,
+};
 
-  const prestigePageData = {
-    publications,
-    awards,
-  }
-
-  export default prestigePageData;
+export default prestigePageData;
