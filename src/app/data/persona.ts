77fc5f4c6280100e7018ebb1ed033b1924bf4b32
Suvadra-Barua <suvadra@universalmachine.io>
2024-09-08 20:02:34 +0600

    Add new line in education section

--- src/app/data/persona.ts
+++ src/app/data/persona.ts
@@ -6,10 +6,10 @@ const researchAgenda = `<b>Blockchain, Distributed Systems, Zero Knowledge proof
 const education = `<p>1. <b>Bachelor of Science (B.Sc.), Computer Science and Engineering (CSE)</b> <br>
 <b>CGPA:</b> 3.49/4.00 (Overall), with a strong upward trend to 3.79/4.00 over the last four semesters. <br>
 Chittagong University of Engineering & Technology (CUET)  <br>
-<b> Thesis: </b> A Public Blockchain Scheme for Cryptocurrency with an Efficient Consensus Algorithm<p>
+<b> Thesis: </b> A Public Blockchain Scheme for Cryptocurrency with an Efficient Consensus Algorithm<p> <br>
 <p>2.<b>Higher Secondary Certificate (HSC)</b><br>
 CUET School & College | 2013-2015<br>
-GPA: 5.00 (out of 5.00)</p>
+GPA: 5.00 (out of 5.00)</p> <br>
 <p>3.<b>Secondary School Certificate (SSC)</b><br>
 Bangladesh Navy High School,Kaptai | 2009-2013<br>
 GPA: 5.00 (out of 5.00)</p>`;
