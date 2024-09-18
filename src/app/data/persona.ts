66211dff6b099b37a376cbca61c8c9224a6ac4b4
Suvadra-Barua <suvadra@universalmachine.io>
2024-09-18 20:02:58 +0600

    Update research interests

--- src/app/data/persona.ts
+++ src/app/data/persona.ts
@@ -2,7 +2,7 @@ const title = "Suvadra Barua";
 const position = "Software Engineer";
 const aboutme =
   "With over 2 years of experience in designing, developing, and deploying decentralized applications (DApps) on Celo, Ethereum, and other blockchain platforms, my approach to project selection is centered around continuous learning and adaptability. I don't limit myself to projects based solely on existing knowledge; instead, I actively seek opportunities for acquiring new skills and insights. I engage in frontend, backend, ML etc. development tasks, demonstrating a willingness to explore and learn new areas. While I may not consider myself a pro in every aspect, I thrive on the challenge of mastering new technologies. Additionally, I have a proven ability to mentor and educate others about blockchain technology and smart contracts.";
-const researchAgenda = `<b>Blockchain, Distributed Systems, Zero Knowledge proof, Quantum-Resistant Cryptography</b>`;
+const researchAgenda = `<b>My research interests center around security and privacy, with a specialized focus on Blockchain, Distributed Systems, Zero-Knowledge Proofs, and Post-Quantum Cryptography. I aim to explore the intersection of these fields to develop innovative solutions that enhance the security and resilience of decentralized systems in the post-quantum era.</b>`;
 const education = `<p>1. <b>Bachelor of Science (B.Sc.), Computer Science and Engineering (CSE)</b> <br>
 <b>CGPA:</b> 3.49/4.00 (Overall), with a strong upward trend to 3.79/4.00 over the last four semesters. <br>
 Chittagong University of Engineering & Technology (CUET)  <br>
